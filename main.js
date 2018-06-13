ef53f8c75bc98bc22e4b10755e8f7fe865643e5d
Ryan Borja <ryan.t.bortja@gmail.com>
2018-06-13 10:18:00 -0400

    added score and high score

--- main.js
+++ main.js
@@ -2,24 +2,30 @@ var game = document.getElementById('game');
 var ctx = game.getContext('2d');
 var sprites = new Image();
 sprites.src = 'assets/frogger-sprites.png';
+var score = 0;
+var highScore = 0;
 
 function paint() {
 
     ctx.fillStyle = '#4d94ff';
-    ctx.fillRect(0, 40, 600, 240);
+    ctx.fillRect(0, 40, 440, 240);
 
     ctx.fillStyle = '#404040';
-    ctx.fillRect(0, 320, 600, 200);
+    ctx.fillRect(0, 320, 440, 200);
 
     ctx.drawImage(sprites, 0, 120, 399, 35, 0, 520, 440, 44);
     ctx.drawImage(sprites, 0, 120, 399, 35, 0, 280, 440, 44);
 
     ctx.fillStyle = '#000';
-    ctx.fillRect(0, 0, 600, 40);
-    ctx.fillRect(0, 560, 600, 40);
+    ctx.fillRect(0, 0, 440, 40);
+    ctx.fillRect(0, 560, 440, 40);
 
-    ctx.font = '48px serif';
-    ctx.strokeText('Hello world', 50, 100);
+    ctx.font = '25px VT323';
+    ctx.fillStyle = "white";
+    ctx.fillText('SCORE', 10, 18);
+    ctx.fillText('HIGH SCORE', 300, 18);
+    ctx.fillText(''+ score +'',10, 35);
+    ctx.fillText(''+highScore+'', 300, 35);
 }
 
 paint();
